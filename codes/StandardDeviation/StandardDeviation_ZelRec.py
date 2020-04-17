df8c473f53a0da7c2d9eb4214c92ba2b735dc843
FedeDavilaKurban <fdavilakurban@unc.edu.ar>
2020-04-17 12:02:19 -0300

    changes to read files with sm0.3

--- codes/StandardDeviation/StandardDeviation_ZelRec.py
+++ codes/StandardDeviation/StandardDeviation_ZelRec.py
@@ -1,12 +1,14 @@
 ########################################################################################	
+zrdir = '/home/fede/mnt/clemente/HomDist/Multipoles/data/out/zelrec/'
+
 xi0_zr = [] # Monopole
 xi2_zr = [] # Quadrupole
 xi4_zr = [] # Hexadecapole
-N_zr = len(glob.glob('../../data/out/zelrec/*zelrec*{}*_clem*'.format(Nran))) # glob.glob works like "ls"
-print('Number of xi_zelrec is: {} (should be 200)'.format(N))
+N_zr = len(glob.glob(zrdir+'*zelrec*{}*_sm{}*'.format(Nran,sm))) # glob.glob works like "ls"
+print('Number of xi_zelrec is: {} (should be 200)'.format(N_zr))
 for iN in range(0,2*N_zr,2):
 	# "xil" meaning $\xi_l$, aka multipoles
-	xil_zr = ascii.read('../../data/out/zelrec/xi_l_zelrec_{}_{}-{}_clem.txt'.format(Nran,iN,iN+1),names=['xi_0','xi_2','xi_4','xi_6'])
+	xil_zr = ascii.read(zrdir+'xi_l_zelrec_{}_{}-{}_sm{}.txt'.format(Nran,iN,iN+1,sm),names=['xi_0','xi_2','xi_4','xi_6'])
 	xi0_zr.append( xil_zr['xi_0'].data )
 	xi2_zr.append( xil_zr['xi_2'].data )
 	xi4_zr.append( xil_zr['xi_4'].data )
@@ -23,7 +25,7 @@ for ir in range(len(nr)):
 
 std_zr = Table([std_xi0_zr,std_xi2_zr,std_xi4_zr,nr], names=('xi0', 'xi2','xi4','r'))
 
-fname_zr = '../../data/out/std_xi/std_zr_{}.txt'.format(Nran)
+fname_zr = '/home/fede/Proyectos/Multipoles/data/out/std_xi/std_zr_{}.txt'.format(Nran)
 ascii.write(std_zr, fname_zr, overwrite=True)
 print('Created {}'.format(fname_zr))
 #########################################################################################	
--- codes/testsm/pks.py
+++ codes/testsm/pks.py
@@ -9,36 +9,43 @@ from nbodykit.lab import *
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import glob
 
 box=1.
 dk=0.7
 
+nran = 1*87**3
 zr_dir = '/home/fede/mnt/clemente2/8x87_testsm/'
-plot_dir= os.path.join('/home/fede/Proyectos/Multipoles/plots/testsm/')
+#zr_dir = '/home/fede/mnt/clemente2/8x87/'
+plot_dir= '/home/fede/Proyectos/Multipoles/plots/testsm/'
 
 
 
-files = os.listdir(zr_dir)
+#files = glob.glob(zr_dir+'/*sm0.3*')
+files = glob.glob(zr_dir+'/*')
 files.sort()
+#files = files[:2]
 
 fig, axes = plt.subplots(nrows=1, ncols=10, sharey=True, figsize=(20, 4))
 
 for i in range(10):
+#for i in range(1):
 
 	f1 = files[i]
 	f2 = files[i+10]
+	#f2 = files[i+1]
 	sm = f1[-7:-4]
 	
 	print(f1,f2)
 	
-	zr1=Table(np.load(zr_dir+f1),names=['x','y','z']) 
+	zr1=Table(np.load(f1),names=['x','y','z']) 
 	zcat1=ArrayCatalog(zr1)
 	zcat1['Position'] = transform.StackColumns(zcat1['x'], zcat1['y'], zcat1['z'])
 	real_mesh = zcat1.to_mesh(compensated=True, resampler='tsc', position='Position', BoxSize=[box,box,box],Nmesh=512)
 	r = FFTPower(real_mesh, mode='1d',dk=dk)
 	Pkzelrec1 = r.power
 
-	zr2 = Table(np.load(zr_dir+f2),names=['x','y','z']) 
+	zr2 = Table(np.load(f2),names=['x','y','z']) 
 	zcat2 = ArrayCatalog(zr2)
 	zcat2['Position'] = transform.StackColumns(zcat2['x'], zcat2['y'], zcat2['z'])
 	real_mesh = zcat2.to_mesh(compensated=True, resampler='tsc', position='Position', BoxSize=[box,box,box],Nmesh=512)
@@ -50,8 +57,9 @@ for i in range(10):
 	axes[i].loglog(Pkzelrec1['k'], Pkzelrec1['power'].real,label='ZelRec1',color='red')
 	axes[i].loglog(Pkzelrec2['k'], Pkzelrec2['power'].real,label='ZelRec2',color='red')
 	axes[i].set_title('sm='+sm)
-	axes[i].set_ylim([1E-17,1E-6])
-
+	if nran==8*87**3: axes[i].set_ylim([1E-17,1E-4])
+	if nran==1*87**3: axes[i].set_ylim([1E-15,1E-4])
+	
 
 plt.tight_layout()
 plt.savefig(os.path.join(plot_dir+'pks.png'))
